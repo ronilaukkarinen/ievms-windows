80c2b8fb14e2b3062dcbca386ab55f72e71dac43
Roni Laukkarinen <roni@laukkarinen.info>
2014-06-18 10:21:41 +0300

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 ievms-windows
 =============
 
-Script for batch installing IE Virtualbox Virtual Machines for Windows.
+Requirements
+----
+
+ - GNUWin32 wget for Windows: http://gnuwin32.sourceforge.net/packages/wget.htm
+
+Usage
+----
+
+1. Set PATH (Computer > Properties > Advanced System Settings > Environment Variables > System Variables > Path > Variable value: add `;C:\Program Files (x86)\GnuWin32\bin` in the end of input.
+2. Run ievms_windows.bat
---
+++ ievms_windows.bat
@@ -0,0 +1,48 @@
+C:
+cd C:\
+mkdir ievms
+cd ievms
+mkdir IE7.Vista
+cd IE7.Vista
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE7_Vista/Windows/IE7.Vista.For.WindowsVirtualBox.part001.exe
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE7_Vista/Windows/IE7.Vista.For.WindowsVirtualBox.part002.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE7_Vista/Windows/IE7.Vista.For.WindowsVirtualBox.part003.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE7_Vista/Windows/IE7.Vista.For.WindowsVirtualBox.part004.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE7_Vista/Windows/IE7.Vista.For.WindowsVirtualBox.part005.rar
+IE7.Vista.For.WindowsVirtualBox.part001.exe
+cd ..
+mkdir IE8.Windows7
+cd IE8.Windows7
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_Win7/Windows/IE8.Win7.For.WindowsVirtualBox.part001.exe
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_Win7/Windows/IE8.Win7.For.WindowsVirtualBox.part002.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_Win7/Windows/IE8.Win7.For.WindowsVirtualBox.part003.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_Win7/Windows/IE8.Win7.For.WindowsVirtualBox.part004.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE8_Win7/Windows/IE8.Win7.For.WindowsVirtualBox.part005.rar
+IE8.Win7.For.WindowsVirtualBox.part001.exe
+cd ..
+mkdir IE9.Windows7
+cd IE9.Windows7
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Windows/IE9.Win7.For.WindowsVirtualBox.part001.exe
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Windows/IE9.Win7.For.WindowsVirtualBox.part002.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Windows/IE9.Win7.For.WindowsVirtualBox.part003.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Windows/IE9.Win7.For.WindowsVirtualBox.part004.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE9_Win7/Windows/IE9.Win7.For.WindowsVirtualBox.part005.rar
+IE9.Win7.For.WindowsVirtualBox.part001.exe
+cd ..
+mkdir IE10.Windows7
+cd IE10.Windows7
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Windows/IE10.Win7.For.WindowsVirtualBox.part001.exe
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Windows/IE10.Win7.For.WindowsVirtualBox.part002.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Windows/IE10.Win7.For.WindowsVirtualBox.part003.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Windows/IE10.Win7.For.WindowsVirtualBox.part004.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE10_Win7/Windows/IE10.Win7.For.WindowsVirtualBox.part005.rar
+IE10.Win7.For.WindowsVirtualBox.part001.exe
+cd ..
+mkdir IE11.Windows7
+cd IE11.Windows7
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Windows/IE11.Win7.For.WindowsVirtualBox.part001.exe
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Windows/IE11.Win7.For.WindowsVirtualBox.part002.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Windows/IE11.Win7.For.WindowsVirtualBox.part003.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Windows/IE11.Win7.For.WindowsVirtualBox.part004.rar
+wget --no-check-certificate https://az412801.vo.msecnd.net/vhd/VMBuild_20131127/VirtualBox/IE11_Win7/Windows/IE11.Win7.For.WindowsVirtualBox.part005.rar
+IE11.Win7.For.WindowsVirtualBox.part001.exe
